545bbebe9075177ca3fee073d4d1255aadc15b0d
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-06-29 22:17:33 +0530

    enabling eureca server

--- src/main/java/com/eazybytes/cards/CardsApplication.java
+++ src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -4,7 +4,9 @@ import com.eazybytes.cards.dto.CardsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties(value = {CardsContactInfoDto.class})
 public class CardsApplication {
