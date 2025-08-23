6b73f675fe66b1e59b9ef634d7d36ec23db81747
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-08-23 13:43:47 +0530

    changes

--- src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -101,8 +101,9 @@ public class CardsController {
                                                      @RequestParam
                                                      @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
                                                      String mobileNumber) {
-        log.debug("eazyBank-correlation-id found:{}", correlationId);
+        log.debug("eazyBank-correlation-id found: fetchCardDetails() started");
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        log.debug("eazyBank-correlation-id found: fetchCardDetails() ended");
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 
